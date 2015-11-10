55258ec515cf3e20d19f8e5c2d76c52d3a42acb4
Dominique <deejay.whitegrass@gmail.com>
2015-11-10 16:46:57 -0600

    completed project. Extra stuff not done

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-angular.module('directivePractice', []);
+var app = angular.module('directivePractice', []);
+
+app.directive('lessonHider', function() {
+	return {
+		templateUrl: 'lessonHider.html',
+		restrict: 'E',
+		scope: {
+			topic: '=',
+			dayAlert: '&',
+		},
+		link: function(scope, element, attributes) {
+			scope.getSchedule.then(function(res) {
+				scope.schedule= res.data;		
+
+				scope.schedule.forEach(function( scheduleDay ) {
+						if (scheduleDay.lesson === scope.topic) {
+							element.css('text-decoration', 'line-through');
+							scope.lessonDay = scheduleDay.weekday;
+							return;
+						}
+					});
+
+			});
+		},
+		controller: function($scope, lessonService) {
+			$scope.getSchedule = lessonService.getSchedule();
+		}
+	};
+});
